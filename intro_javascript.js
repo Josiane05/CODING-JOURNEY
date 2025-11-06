4f40d34cf9ada346cbfe389ba2c1fa456602c9e1
Josiane05 <josianemushimiyimana05@gmail.com>
2025-11-06 10:35:26 +0200

    datatypes in javascript

--- intro_javascript.js
+++ intro_javascript.js
@@ -3,3 +3,21 @@
 console.log(20);
 console.log('it was a love at first time');
 */
+let x;
+console.log(x);
+
+let emptyvalue = null;
+console.log(null);
+
+console.log('woooh i love to code!');
+console.log(20.90);
+let bigNumber = 1234567890123456;
+console.log(bigNumber);
+let isStudent = true;
+console.log(isStudent);
+let person = { name: "John", age: 25 };
+   let array = [1, 2, 3];
+   let date = new Date();
+   console.log(person);
+   console.log(array);
+   console.log(date);
